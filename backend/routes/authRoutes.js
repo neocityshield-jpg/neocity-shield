f125dd8b8c6562408945825ee4059d592ee1ba97
neocityshield-jpg <neocityshield@gmail.com>
2026-03-23 21:58:43 -0500

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,7 +4,7 @@ const authCtrl = require('../controllers/authController');
 const { verificarToken, verificarRol } = require('../middleware/authMiddleware');
 
 router.post('/login',    authCtrl.login);
-router.post('/registro', verificarToken, verificarRol('gerencia'), authCtrl.registro);
+router.post('/registro', authCtrl.registro);
 router.get('/perfil',    verificarToken, authCtrl.perfil);
 
 module.exports = router;
