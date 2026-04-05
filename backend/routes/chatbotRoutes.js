6a90df7381c01c1c126dd6f78bf37a22a31a5ba5
neocityshield-jpg <neocityshield@gmail.com>
2026-04-04 21:47:28 -0500

    Update chatbotRoutes.js

--- backend/routes/chatbotRoutes.js
+++ backend/routes/chatbotRoutes.js
@@ -3,6 +3,6 @@ const router      = express.Router();
 const chatbotCtrl = require('../controllers/chatbotController');
 const { verificarToken } = require('../middleware/authMiddleware');
 
-router.post('/mensaje', verificarToken, chatbotCtrl.enviarMensaje);
+router.post('/mensaje', chatbotCtrl.enviarMensaje);
 
 module.exports = router;
