d713b5f2b515111d74e090b1bc6a71cb3ba4e931
neocityshield-jpg <neocityshield@gmail.com>
2026-04-24 14:55:39 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,10 @@ const pool = new Pool({
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port:     process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }
+  port:     process.env.DB_PORT || 5432,
+  ssl: process.env.DB_SSL === 'true'
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 module.exports = pool;
